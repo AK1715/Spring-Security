f1807294ecd81d861d21bd9aeb4660d1a30a1b35
AK1715 <ns031077@gmail.com>
2026-01-07 18:27:48 +0530

    spring

---
+++ src/main/java/com/spring_security/securityConfig.java
@@ -0,0 +1,34 @@
+package com.spring_security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class securityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+        http.authorizeHttpRequests(authorizeRequest ->
+                authorizeRequest.anyRequest().authenticated());
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("akshay")
+                .password("{noop}akshay1")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+}
