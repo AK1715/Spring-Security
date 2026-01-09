a6252aad7a988ec75d5881d2e83ae49193e1bbff
AK1715 <ns031077@gmail.com>
2026-01-09 18:55:41 +0530

    new file jwtUtils

--- src/main/java/com/spring_security/JwtUtils.java
+++ src/main/java/com/spring_security/JwtUtils.java
@@ -3,7 +3,6 @@ package com.spring_security;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -12,15 +11,15 @@ import java.util.Date;
 @Component
 public class JwtUtils {
 
-    private String jwtSecret = "a-string-secret-at-least-256-bits-long";
-    private int jwtExpirations = 172800000;
+    final String jwtSecret = "YS1zdHJpbmctc2VjcmV0LWF0LWxlYXN0LTI1Ni1iaXRzLWxvbmc=";
+    final int jwtExpirations = 172800000;
 
     public String getJwtFromHeader(){
         return "";
     }
 
-    public String generateTokenFromHeader(String userName){
-        return Jwts.builder()
+    public String generateTokenFromUsername(String userName){
+        return  Jwts.builder()
                 .subject(userName)
                 .issuedAt(new Date())
                 .expiration(new Date(new Date().getTime() + jwtExpirations))
