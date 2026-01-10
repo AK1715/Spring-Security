39e0c1ad5dcf607da1839450c48b11a7db24bdbf
AK1715 <ns031077@gmail.com>
2026-01-10 13:33:05 +0530

    New change

--- src/main/java/com/spring_security/JwtUtils.java
+++ src/main/java/com/spring_security/JwtUtils.java
@@ -1,10 +1,15 @@
 package com.spring_security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 
@@ -14,20 +19,37 @@ public class JwtUtils {
     final String jwtSecret = "YS1zdHJpbmctc2VjcmV0LWF0LWxlYXN0LTI1Ni1iaXRzLWxvbmc=";
     final int jwtExpirations = 172800000;
 
-    public String getJwtFromHeader(){
-        return "";
+    public String getJwtFromHeader(HttpServletRequest request){
+        String bearerToken = request.getHeader("Authorization");
+        if(bearerToken != null && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7);
+        }
+        else {
+        return "Null";
+        }
     }
 
-    public String generateTokenFromUsername(String userName){
+    public String generateTokenFromUsername(UserDetails userDetails){
+        String userName = userDetails.getUsername();
+
         return  Jwts.builder()
                 .subject(userName)
+                .claim("roles", userDetails.getAuthorities().stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .toList())
                 .issuedAt(new Date())
                 .expiration(new Date(new Date().getTime() + jwtExpirations))
                 .signWith(key())
                 .compact();
     }
 
-    public Boolean validateJwtToken(){
+    public boolean validateJwtToken(String jwtToken){
+        try{
+            Jwts.parser().verifyWith((SecretKey) key()).build()
+                    .parseSignedClaims(jwtToken);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
         return true;
     }
 
@@ -35,4 +57,15 @@ public class JwtUtils {
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
     }
 
+    public String getUsernameFromToken(String jwt) {
+        return Jwts.parser().verifyWith((SecretKey) key())
+                .build().parseSignedClaims(jwt)
+                .getPayload().getSubject();
+    }
+
+    public Claims getAllClaims(String jwt) {
+        return Jwts.parser().verifyWith((SecretKey) key())
+                .build().parseSignedClaims(jwt)
+                .getPayload();
+    }
 }
